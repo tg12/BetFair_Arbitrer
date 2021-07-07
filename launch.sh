b83ca0938fe8f332616ee8605f68d0b9dfb2dbd2
tg12 <sawyer12@gmail.com>
2021-07-07 21:54:57 +0000

    fixes

--- launch.sh
+++ launch.sh
@@ -3,4 +3,4 @@ _now=$(date +"%Y_%m_%d_%Hh%M")
 _directory="$HOME/adabet/logs/output_$_now"
 mkdir $_directory
 USERNAME=$(whoami)
-proxychains python $HOME/adabet/main.py $_directory $USERNAME &>$_directory/stdouterr.txt
+proxychains python3 $HOME/adabet/main.py $_directory $USERNAME &>$_directory/stdouterr.txt
